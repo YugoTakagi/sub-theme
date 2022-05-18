7002fddf29fa1f9ac7c3c17572c91aa7d28cd115
yugo <u.riri.yuyu@gmail.com>
2022-05-18 18:50:16 +0900

    change pbs

--- PBS_run.sh
+++ PBS_run.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/tcsh
 #PBS -q SINGLE
 #PBS -N sub-theme
 #PBS -l select=1
 #PBS -j oe
+
+# for conda
+setenv PATH /home/$USER/.conda/bin:$PATH
+source /home/$USER/.conda/etc/profile.d/conda.csh
+
+#source /home/$USER/.cshrc
+
+conda init tcsh
+conda activate Tensorflow_env
 
 cd ${PBS_O_WORKDIR}
 
